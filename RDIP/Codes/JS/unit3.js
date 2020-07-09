4ba6644f4ef6f93a02d12bcde7b95b9bcdf45577
Siddharth Jain <jainsiddharth7747@gmail.com>
2020-07-10 03:25:32 +0530

    defined functionality of check correctness of sentence button

--- RDIP/Codes/JS/unit3.js
+++ RDIP/Codes/JS/unit3.js
@@ -1,21 +1,21 @@
-var english = [["John ate an apple before afternoon", "before afternoon John ate an apple", "John before afternoon ate an apple"], 
-			   ["some students like to study in the night", "at night some students like to study"], 
-			   ["John and Mary went to church", "Mary and John went to church"], 
-			   ["John went to church after eating", "after eating John went to church", "John after eating went to church"], 
-			   ["did he go to market", "he did go to market"], 
-			   ["the woman who called my sister sells cosmetics", "the woman who sells cosmetics called my sister", "my sister who sells cosmetics called the woman", "my sister who called the woman sells cosmetics"],
-			   ["John goes to the library and studies", "John studies and goes to the library"],
-			   ["John ate an apple so did she", "she ate an apple so did John"],
-			   ["the teacher returned the book after she noticed the error", "the teacher noticed the error after she returned the book", "after the teacher returned the book she noticed the error", "after the teacher noticed the error she returned the book", "she returned the book after the teacher noticed the error", "she noticed the error after the teacher returned the book", "after she returned the book the teacher noticed the error", "after she noticed the error the teacher returned the book"],
-			   ["I told her that I bought a book yesterday", "I told her yesterday that I bought a book", "yesterday I told her that I bought a book", "I bought a book that I told her yesterday", "I bought a book yesterday that I told her", "yesterday I bought a book that I told her"] 			  
+var english = [["John ate an apple before afternoon ", "before afternoon John ate an apple ", "John before afternoon ate an apple "], 
+			   ["some students like to study in the night ", "at night some students like to study "], 
+			   ["John and Mary went to church ", "Mary and John went to church "], 
+			   ["John went to church after eating ", "after eating John went to church ", "John after eating went to church "], 
+			   ["did he go to market ", "he did go to market "], 
+			   ["the woman who called my sister sells cosmetics ", "the woman who sells cosmetics called my sister ", "my sister who sells cosmetics called the woman ", "my sister who called the woman sells cosmetics "],
+			   ["John goes to the library and studies ", "John studies and goes to the library "],
+			   ["John ate an apple so did she ", "she ate an apple so did John "],
+			   ["the teacher returned the book after she noticed the error ", "the teacher noticed the error after she returned the book ", "after the teacher returned the book she noticed the error ", "after the teacher noticed the error she returned the book ", "she returned the book after the teacher noticed the error ", "she noticed the error after the teacher returned the book ", "after she returned the book the teacher noticed the error ", "after she noticed the error the teacher returned the book "],
+			   ["I told her that I bought a book yesterday ", "I told her yesterday that I bought a book ", "yesterday I told her that I bought a book ", "I bought a book that I told her yesterday ", "I bought a book yesterday that I told her ", "yesterday I bought a book that I told her "] 			  
 			  ];
-var hindi = [["राम और श्याम बाजार गयें", "राम और श्याम गयें बाजार", "बाजार गयें राम और श्याम", "गयें बाजार राम और श्याम"],
-			 ["राम सोया और श्याम भी", "श्याम सोया और राम भी", "सोया श्याम और राम भी", "सोया राम और श्याम भी"],
-			 ["मैंने उसे बताया कि राम सो रहा है", "मैंने उसे बताया कि सो रहा है राम", "उसे मैंने बताया कि राम सो रहा है", "उसे मैंने बताया कि सो रहा है राम", "मैंने बताया उसे कि राम सो रहा है", "मैंने बताया उसे कि सो रहा है राम", "उसे बताया मैंने कि राम सो रहा है", "उसे बताया मैंने कि सो रहा है राम", "बताया मैंने उसे कि राम सो रहा है", "बताया मैंने उसे कि सो रहा है राम", "बताया उसे मैंने कि राम सो रहा है", "बताया उसे मैंने कि सो रहा है राम"],
-			 ["राम खाकर सोया", "खाकर राम सोया", "राम सोया खाकर", "खाकर सोया राम", "सोया राम खाकर", "सोया खाकर राम"],
-			 ["बिल्लियों को मारकर कुत्ता सो गया", "मारकर बिल्लियों को कुत्ता सो गया", "बिल्लियों को मारकर सो गया कुत्ता", "मारकर बिल्लियों को सो गया कुत्ता", "कुत्ता सो गया बिल्लियों को मारकर", "कुत्ता सो गया मारकर बिल्लियों को", "सो गया कुत्ता बिल्लियों को मारकर", "सो गया कुत्ता मारकर बिल्लियों को"],
-			 ["एक लाल किताब वहाँ है", "एक लाल किताब है वहाँ", "वहाँ है एक लाल किताब", "है वहाँ एक लाल किताब"],
-			 ["एक बड़ी सी किताब वहाँ है", "एक बड़ी सी किताब है वहाँ", "बड़ी सी एक किताब वहाँ है", "बड़ी सी एक किताब है वहाँ", "वहाँ है एक बड़ी सी किताब", "वहाँ है बड़ी सी एक किताब", "है वहाँ एक बड़ी सी किताब", "है वहाँ बड़ी सी एक किताब"]
+var hindi = [["राम और श्याम बाजार गयें ", "राम और श्याम गयें बाजार ", "बाजार गयें राम और श्याम ", "गयें बाजार राम और श्याम "],
+			 ["राम सोया और श्याम भी ", "श्याम सोया और राम भी ", "सोया श्याम और राम भी ", "सोया राम और श्याम भी "],
+			 ["मैंने उसे बताया कि राम सो रहा है ", "मैंने उसे बताया कि सो रहा है राम ", "उसे मैंने बताया कि राम सो रहा है ", "उसे मैंने बताया कि सो रहा है राम ", "मैंने बताया उसे कि राम सो रहा है ", "मैंने बताया उसे कि सो रहा है राम ", "उसे बताया मैंने कि राम सो रहा है ", "उसे बताया मैंने कि सो रहा है राम ", "बताया मैंने उसे कि राम सो रहा है ", "बताया मैंने उसे कि सो रहा है राम ", "बताया उसे मैंने कि राम सो रहा है ", "बताया उसे मैंने कि सो रहा है राम "],
+			 ["राम खाकर सोया ", "खाकर राम सोया ", "राम सोया खाकर ", "खाकर सोया राम ", "सोया राम खाकर ", "सोया खाकर राम "],
+			 ["बिल्लियों को मारकर कुत्ता सो गया ", "मारकर बिल्लियों को कुत्ता सो गया ", "बिल्लियों को मारकर सो गया कुत्ता ", "मारकर बिल्लियों को सो गया कुत्ता ", "कुत्ता सो गया बिल्लियों को मारकर ", "कुत्ता सो गया मारकर बिल्लियों को ", "सो गया कुत्ता बिल्लियों को मारकर ", "सो गया कुत्ता मारकर बिल्लियों को "],
+			 ["एक लाल किताब वहाँ है ", "एक लाल किताब है वहाँ ", "वहाँ है एक लाल किताब ", "है वहाँ एक लाल किताब "],
+			 ["एक बड़ी सी किताब वहाँ है ", "एक बड़ी सी किताब है वहाँ ", "बड़ी सी एक किताब वहाँ है ", "बड़ी सी एक किताब है वहाँ ", "वहाँ है एक बड़ी सी किताब ", "वहाँ है बड़ी सी एक किताब ", "है वहाँ एक बड़ी सी किताब ", "है वहाँ बड़ी सी एक किताब "]
 			];
 $("#lang").on("change", function(){
 	document.getElementById("help1").innerHTML = "Form a sentence (Declarative or Interrogative or any other type) from the given words";
@@ -25,6 +25,8 @@ $("#lang").on("change", function(){
 	document.getElementById("help4").innerHTML = "";
 	document.getElementById("formed").innerHTML = "";
 	$("#reform").css("display", "none");
+	$("#check").css("display", "none");
+	document.getElementById("ans").innerHTML = "";
 	var lang = $("#lang").find(":selected").text();
 	if(lang === "---Select Language---") {
 		alert("Select language");
@@ -34,6 +36,7 @@ $("#lang").on("change", function(){
 		var allans = english[eind];
 		var esample = Math.floor(Math.random()*allans.length);
 		var words = allans[esample].split(" ");
+		words.pop();
 		var wlen = words.length;
 		for(i=0; i<wlen; i++) {
 			var n = Math.floor(Math.random()*words.length);
@@ -61,6 +64,7 @@ $("#lang").on("change", function(){
 				document.getElementById("help3").innerHTML = "";
 				document.getElementById("help4").innerHTML = "";
 				document.getElementById("formed").innerHTML = "";
+				document.getElementById("ans").innerHTML = "";
 				$("#reform").css("display", "none");
 				$("#check").css("display", "none");
 				return true;
@@ -69,6 +73,20 @@ $("#lang").on("change", function(){
 			if(form.length-1 == wlen) {
 				$("#check").css("display", "block");
 			}
+			$("#check").click(function(){
+				for(i=0; i<allans.length; i++) {
+					console.log($("#formed").text());
+					console.log(allans[i]);
+					if($("#formed").text() === allans[i]) {
+						document.getElementById("ans").innerHTML = "Right answer!!!";
+						$("#ans").css("color", "#008000");
+						return true;
+					}
+				}
+				document.getElementById("ans").innerHTML = "Wrong answer!!!";
+				$("#ans").css("color", "#FF0000");
+				return false;
+			});
 			return true;	
 		});
 	} else {
@@ -76,6 +94,7 @@ $("#lang").on("change", function(){
 		var allans = hindi[hind];
 		var hsample = Math.floor(Math.random()*allans.length);
 		var words = allans[hsample].split(" ");
+		words.pop();
 		var wlen = words.length;
 		for(i=0; i<wlen; i++) {
 			var n = Math.floor(Math.random()*words.length);
@@ -103,6 +122,7 @@ $("#lang").on("change", function(){
 				document.getElementById("help3").innerHTML = "";
 				document.getElementById("help4").innerHTML = "";
 				document.getElementById("formed").innerHTML = "";
+				document.getElementById("ans").innerHTML = "";
 				$("#reform").css("display", "none");
 				$("#check").css("display", "none");
 				return true;
@@ -111,6 +131,18 @@ $("#lang").on("change", function(){
 			if(form.length-1 == wlen) {
 				$("#check").css("display", "block");
 			}
+			$("#check").click(function(){
+				for(i=0; i<allans.length; i++) {
+					if($("#formed").text() === allans[i]) {
+						document.getElementById("ans").innerHTML = "Right answer!!!";
+						$("#ans").css("color", "#008000");
+						return true;
+					}
+				}
+				document.getElementById("ans").innerHTML = "Wrong answer!!!";
+				$("#ans").css("color", "#FF0000");
+				return false;
+			});
 			return true
 		});
 	}
