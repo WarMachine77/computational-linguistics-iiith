1c776d3f49796a4a1cdb0cce61f53aea0bae315d
Siddharth Jain <jainsiddharth7747@gmail.com>
2020-07-09 15:43:58 +0530

    added language dropdown and display message

---
+++ RDIP/Codes/JS/unit3.js
@@ -0,0 +1,4 @@
+$("#lang").on("change", function(){
+	document.getElementById("help1").innerHTML = "Form a sentence (Declarative or Interrogative or any other type) from the given words";
+	document.getElementById("help2").innerHTML = "(select the buttons in proper order)";
+});
