40d7512f6874c3c09636e5dc61f3211f3596c535
Siddharth Jain <jainsiddharth7747@gmail.com>
2020-07-16 20:17:32 +0530

    changed the color of boxes, answer message is displayed and added continue button

--- RDIP/Codes/JS/unit4.js
+++ RDIP/Codes/JS/unit4.js
@@ -5,19 +5,124 @@ When the mouse tried to climb out of the basket, she could not. She was too fat 
 			  "A wolf carried off a lamb. The lamb said, \" I know you are going to eat me, but before you eat me I would like to hear you play the flute. I have heard that you can play the flute better than anyone else, even the shepherd himself.\" The wolf was so pleased at this that he took out his flute and began to play. When he had done, the lamb insisted him to play once more and the wolf played again. The shepherd and the dogs heard the sound, and they came running up and fell on the wolf and the lamb was able to get back to the flock.",
 			  "A man had a little dog, and he was very fond of it. He would pat its head, and take it on his knee, and talk to it. Then he would give it little bits of food from his own plate. A donkey looked in at the window and saw the man and the dog. \"Why does he not make a pet of me?\" said the donkey. \"It is not fair. I work hard, and the dog only wags its tail, and barks, and jumps on its master's knee. It is not fair.\" Then the donkey said to himself, \"If I do what the dog does, he may make a pet of me.\" So the donkey ran into the room. It brayed as loudly as it could. It wagged its tail so hard that it knocked over a jar on the table. Then it tried to jump on to its master's knee. The master thought the donkey was mad, and he shouted, \"Help! Help!\" Men came running in with sticks, and they beat the donkey till it ran out of the house, and they drove it back to the field. \"I only did what the dog does,\" said the donkey,\" and yet they make a pet of the dog, and they beat me with sticks. It is not fair."
 			 ]
+function tokens(str) {
+	var words = str.split(/[\s.]+/);
+	var token = [];
+	for(i=0; i<words.length; i++) {
+		var temp = words[i].toLowerCase();
+		if(temp==="" || temp===" " || temp==="." || temp==="," || temp==="?" || temp==='"'){
+			continue;
+		}
+		if(temp.includes(".") || temp.includes(",") || temp.includes("?") || temp.includes('"')) {
+			temp = temp.replace(".","");
+			temp = temp.replace(",","");
+			temp = temp.replace("?","");
+			temp = temp.replace(/"/g,"");
+		}
+		token.push(temp);
+	}
+	return token;
+}
+function types(value, index, self) {
+	return self.indexOf(value) === index;
+}			 
 $("#corp").on("change", function(){
+	$("#ans1").css("background-color", "white");
+	$("#ans2").css("background-color", "white");
+	$("#ans1").val("");
+	$("#ans2").val("");
+	$("#out1").css("color", "black");
+	document.getElementById("out1").innerHTML = "";
+	$("#cont").css("display", "none");
 	var corp = $("#corp").find(":selected").text();
 	if(corp === "---Select a corpus---") {
 		alert("Select a corpus");
 	} else if (corp === "Corpus 1") {
 		document.getElementById("corptxt").innerHTML = corpus[0];
 		$("#tab1").css("display", "block");
-
+		$("#subm1").css("display", "block");
+		var notokens = tokens(corpus[0]).length;
+		var notypes = tokens(corpus[0]).filter(types).length;
+		$("#subm1").on("click", function(){
+			var inp1 = $("#ans1").val();
+			var inp2 = $("#ans2").val();
+			if (inp1 == notokens) {
+				$("#ans1").css("background-color", "green");
+			} else {
+				$("#ans1").css("background-color", "red");
+			}
+			if (inp2 == notypes) {
+				$("#ans2").css("background-color", "green");
+			} else {
+				$("#ans2").css("background-color", "red");
+			}
+			if (inp1 == notokens && inp2 == notypes) {
+				$("#out1").css("color", "green");
+				document.getElementById("out1").innerHTML = "Right Answer";
+				$("#cont").css("display", "block");
+			} else {
+				$("#out1").css("color", "red");
+				document.getElementById("out1").innerHTML = "Wrong Answer";
+				$("#cont").css("display", "none");
+			}
+		});
 	} else if (corp === "Corpus 2") {
 		document.getElementById("corptxt").innerHTML = corpus[1];
 		$("#tab1").css("display", "block");
+		$("#subm1").css("display", "block");
+		var notokens = tokens(corpus[1]).length;
+		var notypes = tokens(corpus[1]).filter(types).length;
+		$("#subm1").on("click", function(){
+			var inp1 = $("#ans1").val();
+			var inp2 = $("#ans2").val();
+			if (inp1 == notokens) {
+				$("#ans1").css("background-color", "green");
+			} else {
+				$("#ans1").css("background-color", "red");
+			}
+			if (inp2 == notypes) {
+				$("#ans2").css("background-color", "green");
+			} else {
+				$("#ans2").css("background-color", "red");
+			}
+			if (inp1 == notokens && inp2 == notypes) {
+				$("#out1").css("color", "green");
+				document.getElementById("out1").innerHTML = "Right Answer";
+				$("#cont").css("display", "block");
+			} else {
+				$("#out1").css("color", "red");
+				document.getElementById("out1").innerHTML = "Wrong Answer";
+				$("#cont").css("display", "none");
+			}
+		});	
 	} else {
 		document.getElementById("corptxt").innerHTML = corpus[2];
 		$("#tab1").css("display", "block");
+		$("#subm1").css("display", "block");
+		var notokens = tokens(corpus[2]).length;
+		var notypes = tokens(corpus[2]).filter(types).length;
+		$("#subm1").on("click", function(){
+			var inp1 = $("#ans1").val();
+			var inp2 = $("#ans2").val();
+			if (inp1 == notokens) {
+				$("#ans1").css("background-color", "green");
+			} else {
+				$("#ans1").css("background-color", "red");
+			}
+			if (inp2 == notypes) {
+				$("#ans2").css("background-color", "green");
+			} else {
+				$("#ans2").css("background-color", "red");
+			}
+			if (inp1 == notokens && inp2 == notypes) {
+				$("#out1").css("color", "green");
+				document.getElementById("out1").innerHTML = "Right Answer";
+				$("#cont").css("display", "block");
+			} else {
+				$("#out1").css("color", "red");
+				document.getElementById("out1").innerHTML = "Wrong Answer";
+				$("#cont").css("display", "none");
+			}
+		});	
 	}
 });
